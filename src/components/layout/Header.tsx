41c5e8fedae21548a1616cebd562457618b00321
Young Ryou <ryouyt@gmail.com>
2026-07-16 18:22:49 +1200

    style: add basic css and theme for tailwind css

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,28 +1,34 @@
 export const Header = () => {
   return (
-    <header className="px-8 py-6 border-b border-slate-700">
+    <header className="px-8 py-6 border-b border-border-editor">
       <nav className="flex justify-between items-center max-w-5xl mx-auto">
         <h1 className="text-xl font-bold">
-          <a href="#" className="text-sky-700 hover:underline">
-            Young Ryou
+          <a href="#" className="text-accent-blue hover:underline">
+            Young Ryou1
           </a>
         </h1>
         <ul className="flex gap-6">
           <li>
             <a
               href="#projects"
-              className="hover:text-sky-700 transition-colors"
+              className="hover:text-accent-blue transition-colors"
             >
               Projects
             </a>
           </li>
           <li>
-            <a href="#skills" className="hover:text-sky-700 transition-colors">
+            <a
+              href="#skills"
+              className="hover:text-accent-blue transition-colors"
+            >
               Skills
             </a>
           </li>
           <li>
-            <a href="#contact" className="hover:text-sky-700 transition-colors">
+            <a
+              href="#contact"
+              className="hover:text-accent-blue transition-colors"
+            >
               Contact
             </a>
           </li>
