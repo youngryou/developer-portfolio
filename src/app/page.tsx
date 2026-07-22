b9a77bb2e6a365b7880626e064e5d678248cf67a
Young Ryou <ryouyt@gmail.com>
2026-07-22 13:39:09 +1200

    feat: add skills section and badge component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import BackToTopButton from '@/components/common/BackToTopButton'
 import { AboutSection } from '@/components/sections/AboutSection'
 import { HeroSection } from '@/components/sections/HeroSection'
+import { SkillsSection } from '@/components/sections/SkillsSection'
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <SkillsSection />
       <AboutSection />
       <BackToTopButton />
     </main>
