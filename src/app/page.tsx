68fd95c9d2c05f6b00e3ed2328e0693640bdb981
Young Ryou <ryouyt@gmail.com>
2026-07-22 17:27:06 +1200

    feat: add initial projects section and fix skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import BackToTopButton from '@/components/common/BackToTopButton'
-import { AboutSection } from '@/components/sections/AboutSection'
 import { HeroSection } from '@/components/sections/HeroSection'
+import { ProjectsSection } from '@/components/sections/ProjectsSection'
 import { SkillsSection } from '@/components/sections/SkillsSection'
+import { AboutSection } from '@/components/sections/AboutSection'
+import BackToTopButton from '@/components/common/BackToTopButton'
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <ProjectsSection />
       <SkillsSection />
       <AboutSection />
       <BackToTopButton />
---
+++ src/components/sections/ProjectsSection.tsx
@@ -0,0 +1,12 @@
+export const ProjectsSection = async () => {
+  return (
+    <section
+      id="projects"
+      className="pt-18 pb-12 md:pt-28 md:pb-18 bg-accent-blue/2"
+    >
+      <div className="mb-18 max-w-5xl mx-auto px-6 md:px-8">
+        <h2 className="text-4xl md:text-5xl font-bold mb-12">Projects</h2>
+      </div>
+    </section>
+  )
+}
